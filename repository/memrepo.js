0f55bb311b2339c9e7be3bdc37a1ca40d849d4d1
Pyunghyuk Yoo <yoophi@gmail.com>
2021-12-02 20:54:49 +0900

    ✨ NEW: MemRepo.get

--- repository/memrepo.js
+++ repository/memrepo.js
@@ -8,6 +8,17 @@ class MemRepo {
   list() {
     return this.todoObjects.map((todoObject) => Todo.factory(todoObject));
   }
+
+  get(id) {
+    try {
+      const todoObject = this.todoObjects.filter(
+        (todoObject) => todoObject.id === id
+      )[0];
+      return Todo.factory(todoObject);
+    } catch (e) {
+      return null;
+    }
+  }
 }
 
 module.exports = MemRepo;
--- repository/memrepo.spec.js
+++ repository/memrepo.spec.js
@@ -9,11 +9,28 @@ const getTodosObject = () =>
     is_completed: false,
   }));
 
+const getTodosObjectWithStaticId = () => {
+  let id = 1;
+  return ["Task One", "Task Two", "Task Three", "Task Four"].map((title) => ({
+    id: String(id++),
+    title,
+    is_completed: false,
+  }));
+};
+
 describe("Memory Repository", () => {
   test("Parameter 없이 repo.list() 호출", () => {
     const todoObjects = getTodosObject();
     const repo = new MemRepo(todoObjects);
     const todos = todoObjects.map((todoObject) => Todo.factory(todoObject));
     expect(repo.list()).toEqual(todos);
+  });
+
+  test("repo.get() 호출", () => {
+    const todoObjects = getTodosObjectWithStaticId();
+    const todo = Todo.factory(todoObjects[0]);
+    const repo = new MemRepo(todoObjects);
+    expect(repo.get("1")).toEqual(todo);
+    expect(repo.get("invalid-todo-id")).toEqual(null);
   });
 });
